14ff2cd83e3206c655f469da9f359b527f950e2e
GayathriRajavelu <gayathrirajavelu99@gmail.com>
2026-02-02 14:14:31 +0530

    addded

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:5173",
-      "https://recipeplatform1.netlify.app",];
+      "https://recipeplatform1.netlify.app"],
   })
 );
 
